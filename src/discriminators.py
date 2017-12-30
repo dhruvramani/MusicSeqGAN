ecc26b83c95c3fe306a20578242ca87efcc0566e
dhruvramani <dhruvramani98@gmail.com>
2017-12-31 00:18:23 +0530

    BAP BAPAPAPBAP

--- src/discriminators.py
+++ src/discriminators.py
@@ -2,22 +2,29 @@ import numpy as np
 import tensorflow as tf
 from keras import backend as K
 from keras.layers import LSTM, Dense, Dropout
-
+from keras.models import Sequential
 
 class Discriminator():
     # steps = 20
-    def __init__(self, sess, steps, embedding_dim):
+    def __init__(self, sess, batch_size, steps, embedding_dim, dropout):
         self.sess = sess
-        self.input_x = tf.placeholder(tf.float32, [None, steps, embedding_dim], name="input_x")
-        self.y_orig = tf.placeholder(tf.float32, [None, 1], name="y_orig")
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
-        self.predict = self.model()
+        self.batch_size = batch_size
+        self.steps = steps
+        self.embedding_dim = self.embedding_dim # One hot vector 
+        self.model_run = self.model(dropout)
 
-    def model(self):
-        lstm = LSTM(10, name="main_lstm")(self.input_x)
-        drop = Dropout(self.dropout_keep_prob)(lstm)
-        out = Dense(1)(drop)
-        return out
+    def model(self, dropout):
+        model = Sequential()
+        # TODO : Add embedding layer
+        model.add(LSTM(10, input_shape=[self.steps, self.embedding_dim]))
+        model.add(Dropout(dropout))
+        model.add(Dense(1))
+        return model
 
-    def loss(self):
-        return tf.reduce_sum(tf.log(self.y_orig + 10e-10) + tf.log(1 - self.predict + 10e-10))
+    def predict(self, input_x):
+        return self.model_run(input_x)
+
+    def loss(self, logits):
+        labels = tf.constant(tf.float32, tf.concat([tf.ones(shape = [self.batch_size, 1]), tf.zeros(shape=[self.batch_size, 1])], axis=1))
+        # ALT :  tf.reduce_sum(tf.log(labels + 10e-10) + tf.log(1 - predictions + 10e-10))
+        return tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
---
+++ src/generator.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+class Generator(object):
+    def __init__(self, sess, batch_size, steps, embedding_dim, dropout):
+        # self.sess = sess
+        self.batch_size = batch_size
+        self.steps = step
+        self.embedding_dim = embedding_dim
+        self.model_run = self.model(dropout)
+
+    def model(self, dropout):
+        model = Sequential()
+        model.add(LSTM(32, return_sequences=True, input_shape=(self.steps, self.embedding_dim)))
+        model.add(LSTM(32, return_sequences=True))
+        model.add(Dense(self.embedding_dim ,activation="softmax"))
+        return model
+
+    def predict(self, input):
+        return self.model_run(input)
+
+    def loss(self, predictons):
